d756c90b0b2fefd8a706628adbe3d46f69d0565d
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-08-31 17:29:06 +0300

    add fix conflict with git

--- Task_43-HW6/Program.cs
+++ Task_43-HW6/Program.cs
@@ -12,11 +12,6 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите k2: ");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-
-double x = (b2 - b1) / (k1 - k2);
-double y = (k1*b2 - k2*b1) / (k1 - k2);
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
-=======
 if (k1 == k2 && b1 != b2)
 {
     Console.WriteLine($"прямые парралельны и различны. Точки пересечения нет.");
